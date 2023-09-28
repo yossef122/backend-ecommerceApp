e9c74f8e0f323e71a34b784f6c58fed302c3b82d
youssef helmy <96450513+yossef122@users.noreply.github.com>
2023-09-28 03:47:36 +0300

    add webhook to index.js

--- index.js
+++ index.js
@@ -1,15 +1,83 @@
 // const path = require('path');
 
-const express = require('express');
-const dotenv = require('dotenv');
-const morgan = require('morgan');
+// const express = require("express");
+// const dotenv = require("dotenv");
+// const morgan = require("morgan");
+// const compression = require("compression");
+// const cors = require("cors");
 
-dotenv.config({ path: 'config.env' });
-const ApiError = require('./utils/apiError');
-const globalError = require('./middlewares/errorMiddleware');
-const dbConnection = require('./config/database');
+// dotenv.config({ path: "config.env" });
+// const ApiError = require("./utils/apiError");
+// const globalError = require("./middlewares/errorMiddleware");
+// const dbConnection = require("./config/database");
+// // Routes
+// const mountRoutes = require("./routes");
+
+// const { webhookCheckout } = require("./services/orderService");
+// // Connect with db
+// dbConnection();
+
+// // express app
+// const app = express();
+
+// // compress all responses
+// app.use(compression());
+// app.use(cors());
+
+// app.post(
+//   "/webhook-checkout",
+//   express.raw({ type: "application/json" }),
+//   webhookCheckout
+// );
+// // Middlewares
+// app.use(express.json());
+
+// if (process.env.NODE_ENV === "development") {
+//   app.use(morgan("dev"));
+//   console.log(`mode: ${process.env.NODE_ENV}`);
+// }
+
+// // Mount Routes
+// mountRoutes(app);
+
+// app.all("*", (req, res, next) => {
+//   next(new ApiError(`Can't find this route: ${req.originalUrl}`, 400));
+// });
+
+// app.use(express.static(path.join(__dirname, "uploads")));
+
+// // Global error handling middleware for express
+// app.use(globalError);
+
+// const PORT = process.env.PORT || 8000;
+// const server = app.listen(PORT, () => {
+//   console.log(`App running running on port ${PORT}`);
+// });
+
+// // Handle rejection outside express
+// process.on("unhandledRejection", (err) => {
+//   console.error(`UnhandledRejection Errors: ${err.name} | ${err.message}`);
+//   server.close(() => {
+//     console.error(`Shutting down....`);
+//     process.exit(1);
+//   });
+// });
+
+const path = require("path");
+
+const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const cors = require("cors");
+const compression = require("compression");
+
+dotenv.config({ path: "config.env" });
+const ApiError = require("./utils/apiError");
+const globalError = require("./middlewares/errorMiddleware");
+const dbConnection = require("./config/database");
 // Routes
-const mountRoutes = require('./routes');
+const mountRoutes = require("./routes");
+const { webhookCheckout } = require("./services/orderService");
 
 // Connect with db
 dbConnection();
@@ -17,19 +85,33 @@ dbConnection();
 // express app
 const app = express();
 
+// Enable other domains to access your application
+app.use(cors());
+app.options("*", cors());
+
+// compress all responses
+app.use(compression());
+
+// Checkout webhook
+app.post(
+  "/webhook-checkout",
+  express.raw({ type: "application/json" }),
+  webhookCheckout
+);
+
 // Middlewares
 app.use(express.json());
-// app.use(express.static(path.join(__dirname, 'uploads')));
+app.use(express.static(path.join(__dirname, "uploads")));
 
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
   console.log(`mode: ${process.env.NODE_ENV}`);
 }
 
 // Mount Routes
 mountRoutes(app);
 
-app.all('*', (req, res, next) => {
+app.all("*", (req, res, next) => {
   next(new ApiError(`Can't find this route: ${req.originalUrl}`, 400));
 });
 
@@ -42,7 +124,7 @@ const server = app.listen(PORT, () => {
 });
 
 // Handle rejection outside express
-process.on('unhandledRejection', (err) => {
+process.on("unhandledRejection", (err) => {
   console.error(`UnhandledRejection Errors: ${err.name} | ${err.message}`);
   server.close(() => {
     console.error(`Shutting down....`);
